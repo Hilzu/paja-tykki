dbc04bfcab8a5802852199abdcc00b5bf3901053
Santeri Hiltunen <hiltunen.santeri@gmail.com>
2015-11-08 18:16:40 +0200

    Fetch sub pages too

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const sourceUrl = 'http://www.iltalehti.fi'
 const userAgent = 'User-Agent	Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/601.2.7 (KHTML, like Gecko) Version/9.0.1 Safari/601.2.7'
 
 app.all('*', (req, res, next) => {
-  fetch(sourceUrl, { headers: { 'User-Agent': userAgent } })
+  fetch(sourceUrl + req.originalUrl, { headers: { 'User-Agent': userAgent } })
     .then((res) => res.text())
     .then(pajaify)
     .then((pajareissa) => res.send(pajareissa))
