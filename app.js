cb3d58f3be4a18d4d117a78edbe28a041a54e248
Santeri Hiltunen <hiltunen.santeri@gmail.com>
2016-04-24 10:53:35 +0300

    Force desktop site on mobile to prevent redirect to page without pajarit

--- app.js
+++ app.js
@@ -7,12 +7,19 @@ app.set('x-powered-by', false)
 
 const sourceUrl = 'http://www.iltalehti.fi'
 const userAgent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_4) AppleWebKit/601.5.17 (KHTML, like Gecko) Version/9.1 Safari/601.5.17'
+const expireTime = new Date()
+expireTime.setFullYear(expireTime.getFullYear() + 1)
+const preventMobileSiteCookie = `ILChooseMobile=false; expires=${expireTime.toUTCString()}; domain=.iltalehti.fi; path=/`
 
 app.all('*', (req, res, next) => {
-  fetch(sourceUrl + req.originalUrl, { headers: { 'User-Agent': userAgent } })
+  fetch(sourceUrl + req.originalUrl, { headers: { 'User-Agent': userAgent, Cookie: preventMobileSiteCookie } })
     .then((res) => res.text())
     .then(pajaify)
-    .then((pajareissa) => res.send(pajareissa))
+    .then((pajareissa) => {
+      res.header('Set-Cookie', preventMobileSiteCookie)
+      res.cookie('ILChooseMobile', 'false', { domain: req.hostname, expires: expireTime })
+      res.send(pajareissa)
+    })
     .catch((err) => next(err))
 })
 
